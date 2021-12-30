bb73bf2f5698a8e7bdd63f6e98240e7a3f5f5851
tjduigna <thomas.j.duignan@gmail.com>
2021-12-29 22:11:15 -0500

    feat: use _version.py

--- automata/__init__.py
+++ automata/__init__.py
@@ -1,9 +1,2 @@
-try:
-    from importlib.metadata import version, PackageNotFoundError
-except ImportError:
-    from importlib_metadata import version, PackageNotFoundError
+from ._version import version as __version__
 
-try:
-    __version__ = version("automata")
-except PackageNotFoundError:
-    __version__ = None
